85b0e36ae7f3be0ad8873af5aa70099524edcd19
Tamrell <angelogroot@hotmail.com>
2020-05-13 13:49:45 +0200

    ay

--- codebase/evaluation.py
+++ codebase/evaluation.py
@@ -5,7 +5,7 @@ import time
 import os
 from progressbar import progressbar
 
-def ndcg(gt_dict, pred_df, at=5):
+def ndcg(pred_df, at=5):
     """Calculates the normalized discounted cumulative gain (by default: at 5)
         Args:
             - gt_fg (DataFrame): dataframe with ground truth ordering
@@ -15,6 +15,7 @@ def ndcg(gt_dict, pred_df, at=5):
             - NDCG (float): normalized discounted cumulative gain as described
                             in blog post
     """
+    gt = load_ground_truth()
     ndcg_list = []
     logs = {i:np.log2(i+2) for i in range(at)}
 
@@ -47,8 +48,7 @@ def load_predictions(path=os.path.join("benchmark_results", "benchmark V0 predic
 if __name__ == '__main__':
     # Load predictions and ground truth
     pred = load_predictions()
-    gt = load_ground_truth()
 
     # print results for the predictions
-    print(ndcg(gt, pred))
+    print(ndcg(pred))
     # print(ndcg(gt, pred2))
